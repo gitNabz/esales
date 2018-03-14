8ad1c7811db9e394220c81d67799ccad371e7f56
gitNabz <nabzgc@gmail.com>
2018-03-14 14:00:49 +0100

    update

--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -10,68 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class UsersController extends Controller
 {
     /**
-     * @Route("/users", name="users.index")
+     * @Route("/register", name="register")
      */
-    public function indexAction()
+    public function registerAction()
     {
-        // Récupérer dans un premier temps la liste des utilisateurs
-        // $pdo = new \PDO('mysql:host=localhost;dbname=symfony', 'root', 'troiswa');
-        // $query = $pdo->prepare('SELECT * FROM users');
-        // $query->execute();
-        // $users = $query->fetchAll(\PDO::FETCH_ASSOC);
-
-        // Récupérer la liste des utilisateurs avec doctrine
-        $repository = $this->getDoctrine()->getRepository('AppBundle\Entity\User');
-        $users = $repository->findAll();
-
-        // Récupérer un utilisateur à partir de son id avec doctrine
-        $user = $repository->find(5); // Récupère l'utilisateur numéro 5
-
-        
-
-        // On charge la vue avec les données récupérées
-        return $this->render('users/index.html.twig', [
-            'users' => $users
-        ]);
-    }
-
-        /**
-     * @Route("/users/{id}", name="users.show", requirements={"id"="\d+"})
-     */
-    public function userAction($id)
-    {
-        // Récupérer dans un premier temps la liste des utilisateurs
-        // $pdo = new \PDO('mysql:host=localhost;dbname=symfony', 'root', 'troiswa');
-        // $query = $pdo->prepare('SELECT * FROM users');
-        // $query->execute();
-        // $users = $query->fetchAll(\PDO::FETCH_ASSOC);
-
-        // Récupérer la liste des utilisateurs avec doctrine
-        $repository = $this->getDoctrine()->getRepository('AppBundle\Entity\User');
-
-        // Récupérer un utilisateur à partir de son id avec doctrine
-        $user = $repository->find($id); // Récupère l'utilisateur numéro 5
-
-        // On charge la vue avec les données récupérées
-        return $this->render('users/users.html.twig', [
-            'user' => $user
-        ]);
-    }
-
-/**
-     * @Route("/users/{name}", name="users.name")
-     */
-    public function usernameAction($name)
-    {
-        // Récupérer la liste des utilisateurs avec doctrine
-        $repository = $this->getDoctrine()->getRepository('AppBundle\Entity\User');
-
-        // Récupérer un utilisateur à partir de son id avec doctrine
-        $user = $repository->findOneByLastname($name); // Récupère l'utilisateur numéro 5
-
-        // On charge la vue avec les données récupérées
-        return $this->render('users/users.html.twig', [
-            'user' => $user
-        ]);
+        return $this->render('users/register.html.twig');
     }
 }
